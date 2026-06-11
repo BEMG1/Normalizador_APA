a09e59af64a5ca73f2a9835cc7e68f63d8532200
Esteban <136500531+BEMG1@users.noreply.github.com>
2026-06-10 19:01:30 -0500

    Fix Repository Name (#31)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Normalizador_APA/',
+  base: '/Citara/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
